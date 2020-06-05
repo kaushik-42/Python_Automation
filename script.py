0bfb5af9f1e6892ac0b50db5a7118f9788fc89e7
Kaushik Tummalapalli <54365583+kaushik-42@users.noreply.github.com>
2020-06-05 23:37:02 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import subprocess
 for i in range(0, 5):
     subprocess.check_call(['python3', 'ex.py'])
 #subprocess is a library that allows our python script to interact with the command line interface or shell.
+#check_call runs an execution in the terminal and waits for the process to end before continuing our script.
 """ where ex.py has 
 print("hello world") and 
 ***the outut will be:***
